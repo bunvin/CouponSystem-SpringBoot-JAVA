34eb38478a8f4fad3142f4c40191f656f897c07e
NaamaL <bunvin@gmail.com>
2024-12-16 15:54:14 +0200

    init
    used https://start.spring.io/
    JDK 23
    SpringBoot 3.3.6
    dependencies are:
    Maven project with dependencies:
    Spring Boot Starter Validation
    Lombok
    Spring Data JPA
    Spring WEB - endless run, contains Apache Tomcat
    MySQL driver

---
+++ demo/src/test/java/JAVA_SpringBoot_CouponSystem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package JAVA_SpringBoot_CouponSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
